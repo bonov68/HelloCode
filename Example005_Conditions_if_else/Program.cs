f8c8f14766490c71510c6fb36eac183c2abc1e29
Antonina <bonov68@mail.ru>
2022-11-22 23:07:42 +0500

    add Example005 Condition_if-else

---
+++ Example005_Conditions_if_else/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("ВВедите ваше имя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")  {
+    Console.WriteLine("МАША_МАША");
+} else {
+    Console.WriteLine("Ты не Маша!!!!!!!");
+}
